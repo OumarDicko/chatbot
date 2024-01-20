080277d5b67d7fd51204864d0f79fd326a29e01e
OumarDicko <103898154+OumarDicko@users.noreply.github.com>
2024-01-20 19:10:02 +0100

    Add files via upload

--- app2.py
+++ app2.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from ultrabot import ultraChatBot
 import json
+import os
 
 app = Flask(__name__)
 historique = []
@@ -13,5 +14,6 @@ def home():
         historique.append(reponseBot)
         return reponseBot
 
-if(__name__) == '__main__':
-    app.run()
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
